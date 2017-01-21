86219a74e779064cf95557f48827170c932f3dbc
Westley Harris <wharris@petsafe.net>
2017-01-21 09:21:10 -0500

    Initial Commit

---
+++ src/Cake.SalesForce/Services/SalesForceService.cs
@@ -0,0 +1,20 @@
+﻿using Cake.SalesForce.SalesForceMetadataApi;
+
+namespace Cake.SalesForce.Services {
+    class SalesForceService {
+        private readonly MetadataPortTypeClient _salesforceClient;
+
+        public SalesForceService() {
+            _salesforceClient = new MetadataPortTypeClient();
+        }
+
+        public void DeployPackage() {
+            var sessionHeader = new SessionHeader();
+            var debuggingHeader = new DebuggingHeader();
+            var callOptions = new CallOptions();
+            var zipFile = new byte[0];
+            var deployOptions = new DeployOptions();
+            _salesforceClient.deploy(sessionHeader, debuggingHeader, callOptions, zipFile, deployOptions);
+        }
+    }
+}

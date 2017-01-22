d9cbf22253cb07c475ed308c9912d8e40926bd08
Westley Harris <wharris@petsafe.net>
2017-01-22 16:32:48 -0500

    Testing some different ways of making SOAP calls to salesforce

--- src/Cake.SalesForce/Services/SalesForceService.cs
+++ src/Cake.SalesForce/Services/SalesForceService.cs
@@ -1,20 +1,32 @@
-﻿using Cake.SalesForce.SalesForceMetadataApi;
+﻿using System;
+using System.IO;
+using System.Net;
+using Cake.SalesForce.SalesforcePartnerApi;
+using Cake.SalesForce.SalesForceMetadataApi;
+using PartnerApi = Cake.SalesForce.SalesforcePartnerApi;
+using MetadataApi = Cake.SalesForce.SalesForceMetadataApi;
 
 namespace Cake.SalesForce.Services {
-    class SalesForceService {
+    public class SalesForceService {
         private readonly MetadataPortTypeClient _salesforceClient;
 
         public SalesForceService() {
             _salesforceClient = new MetadataPortTypeClient();
         }
 
-        public void DeployPackage() {
-            var sessionHeader = new SessionHeader();
-            var debuggingHeader = new DebuggingHeader();
-            var callOptions = new CallOptions();
-            var zipFile = new byte[0];
-            var deployOptions = new DeployOptions();
-            _salesforceClient.deploy(sessionHeader, debuggingHeader, callOptions, zipFile, deployOptions);
+        public void GetSessionId() {
+            //var partnerApiClient = new SoapClient();
+            //var loginResult = partnerApiClient.login(new LoginScopeHeader(), new PartnerApi.CallOptions(), "wharris@petsafe.net", "Charli$@&201604!utzUoAKsUVUtmpM6OOjEBHgcA");
+            using (var client = new WebClient()) {
+                // read the raw SOAP request message from a file=
+                var data = File.ReadAllText("C:/Users/westley/Repos/Cake.SalesForce/src/Cake.SalesForce/SoapRequests/salesforce_soap_login.xml");
+                // the Content-Type needs to be set to XML
+                client.Headers.Add("Content-Type", "text/xml");
+                // The SOAPAction header indicates which method you would like to invoke
+                // and could be seen in the WSDL: <soap:operation soapAction="..." /> element
+                client.Headers.Add("SOAPAction", "''");
+                var response = client.UploadString("https://login.salesforce.com/services/Soap/c/38.0", data);
+            }
         }
     }
 }
---
+++ src/SalesForceTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿using Cake.SalesForce.Services;
+
+namespace SalesForceTesting {
+    class Program {
+        static void Main() {
+            var salesforceService = new SalesForceService();
+            salesforceService.GetSessionId();
+        }
+    }
+}
